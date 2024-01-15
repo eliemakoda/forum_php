ec728ef4392801bc62c008a77736c837ea67b406
eliemakodakowo <eliemakodakowo@gmail.com>
2024-01-16 00:05:36 +0100

    version starter du forum

---
+++ profile.php
@@ -0,0 +1,110 @@
+<html>
+
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Welcome To Forum</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="css/custom.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.html">Forum</a>
+        </div>
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li class="active"><a href="index.html">Home</a></li>
+            <li><a href="register.html">Register</a></li>
+            <li><a href="create.html">Create Topic</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8">
+          <div class="main-col">
+            <div class="block">
+              <h1 class="pull-left">Name</h1>
+              <h4 class="pull-right">A Simple Forum</h4>
+              <div class="clearfix"></div>
+              <hr>
+              <ul id="topics">
+                              <li id="main-topic" class="topic topic">
+                                  <div class="row">
+                                      <div class="col-md-2">
+                                          <div class="user-info">
+                                              <img class="avatar pull-left" src="img/gravatar.png" />
+                                              <ul>
+                                                  <li><strong>username</strong></li>
+                                                  <li><a href="profile.html">Profile</a>
+                                              </ul>
+                                          </div>
+                                      </div>
+                                      <div class="col-md-10">
+                                          <div class="topic-content pull-right">
+                                              <p>
+                                                Globally productize optimal leadership skills without cooperative synergy. Authoritatively promote wireless technologies and high-payoff technology. Conveniently optimize virtual vortals for diverse 
+                                              </p>
+                                          </div>
+
+                                              <a class="btn btn-success" href="" role="button">number of Topics: 22</a>
+                                              <a class="btn btn-primary" href="" role="button">number of replies: 22</a>
+                                          
+                                      </div>
+                                      
+                                  </div>
+                              </li>
+                              
+                              
+              </ul>
+          
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="sidebar">
+            
+            
+            <div class="block">
+            <h3>Categories</h3>
+            <div class="list-group block ">
+              <a href="#" class="list-group-item active">All Topics <span class="badge pull-right">14</span></a> 
+              <a href="#" class="list-group-item">Design<span class="color badge pull-right">4</span></a>
+              <a href="#" class="list-group-item">Development<span class="color badge pull-right">9</span></a>
+              <a href="#" class="list-group-item">Business & Marketing <span class="color badge pull-right">12</span></a>
+              <a href="#" class="list-group-item">Search Engines<span class="color badge pull-right">7</span></a>
+              <a href="#" class="list-group-item">Cloud & Hosting <span class="color badge pull-right">3</span></a>
+            </div>
+            </div>
+  
+            <div class="block" style="margin-top: 20px;">
+              <h3 class="margin-top: 40px">Forum Statistics</h3>
+              <div class="list-group">
+                <a href="#" class="list-group-item">Total Number of Users:<span class="color badge pull-right">4</span></a>
+                <a href="#" class="list-group-item">Total Number of Topics:<span class="color badge pull-right">9</span></a>
+                <a href="#" class="list-group-item">Total Number of Categories: <span class="color badge pull-right">12</span></a>
+                
+              </div>
+              </div>
+            </div>	
+		</div>
+
+ <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+  </body>
+</html>
